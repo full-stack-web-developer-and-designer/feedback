3539ec8048cfdce9096995a453106c9f352dc176
Mirnes Glamočić <contact@mirnesglamocic.com>
2025-07-20 08:33:18 +0200

    Changed JavaScript file location

--- inc/footer.php
+++ inc/footer.php
@@ -133,9 +133,9 @@ echo copyright(2025);
 <button class="floating-btn">
 	Support Me
 </button>
-<script src="panel.js"></script>
+
 </footer>
- 
+<script src="./panel.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
